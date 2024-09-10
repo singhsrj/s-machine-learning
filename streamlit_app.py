582b146533166d0554fc4bb44ef5fbbcc341f892
Suraj Singh <165886618+singhsrj@users.noreply.github.com>
2024-09-10 10:48:39 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,9 @@
 import streamlit as st
-
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as mlt
 st.title('Regression Model ????')
-
 st.write('Hello World!, this is a Multivariate Regression model under construction.Please stand by')
+
+dataset = pd.read_csv('data.csv')
+X=dataset[:,
