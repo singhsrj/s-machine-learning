4d0fcd586a113604b603b87321303012ed7390c1
Suraj Singh <165886618+singhsrj@users.noreply.github.com>
2024-09-10 10:45:37 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
-st.title('Regression Model')
+st.title('Regression Model ????')
 
 st.write('Hello World!, this is a Multivariate Regression model under construction.Please stand by')
